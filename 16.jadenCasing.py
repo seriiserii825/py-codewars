284e1c4a908a6acbadaae57f7bf90bf9bb0bc49f
serii <seriiburduja@gmail.com>
2024-04-18 15:50:14 +0300

    feat: 16

--- 16.jadenCasing.py
+++ 16.jadenCasing.py
@@ -1,4 +1,4 @@
 def jadenCasing(string):
-    return ' '.join(i[0].upper() + i[1:] for i in string.lower().split())
+    return ' '.join(i.capitalize() for i in string.split())
 
-print(jadenCasing("How can mirrors be real if our eyes aren't real"))
+print(jadenCasing("How can mirrors be real if our eYes aren't real"))
