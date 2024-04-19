6c7e4913820b969c57292effc5cdd768db0857c4
serii <seriiburduja@gmail.com>
2024-04-19 15:22:58 +0300

    upd: 23

--- 23.arrayDiff.py
+++ 23.arrayDiff.py
@@ -1,9 +1,4 @@
 def arrayDiff(a, b):
-    result = []
-    for i in a:
-        if i not in b:
-            result.append(i)
-
-    return result
+    return [i for i in a if i not in b]
 
 print(arrayDiff([1,2, 2], [1, 3, 4, 8]))
